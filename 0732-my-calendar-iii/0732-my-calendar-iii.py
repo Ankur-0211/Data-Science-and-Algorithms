b98273016f08b7c75c2416ed850ac79f417d4405
Ankur-0211 <ankurkumar76532@gmail.com>
2026-08-28 16:18:07 +0530

    Time: 863 ms (7.73%), Space: 19.8 MB (31.09%) - LeetHub

---
+++ 0732-my-calendar-iii/0732-my-calendar-iii.py
@@ -0,0 +1,30 @@
+class MyCalendarThree:
+
+    def __init__(self):
+        self.eventmap={}
+        self.maxcount=0
+        
+
+    def book(self, startTime: int, endTime: int) -> int:
+        self.eventmap[startTime]=1+self.eventmap.get(startTime,0)
+        self.eventmap[endTime]=self.eventmap.get(endTime,0)-1
+
+        sortedmap=sorted(self.eventmap.keys())
+        prefix=0
+        for event in sortedmap:
+            
+            if event in self.eventmap:
+                prefix+=self.eventmap[event]
+                self.maxcount=max(self.maxcount,prefix)
+        return self.maxcount
+
+
+
+
+
+        
+
+
+# Your MyCalendarThree object will be instantiated and called as such:
+# obj = MyCalendarThree()
+# param_1 = obj.book(startTime,endTime)
